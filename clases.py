73d6296726f8f8a27fdb1eb18739b58a34b7360a
Adr-dev <adalegre.dev@gmail.com>
2022-01-27 00:11:12 +0100

    Falta incluir las comprobaciones del tablero y los colores de los jugadores.

--- clases.py
+++ clases.py
@@ -2,23 +2,50 @@ from pickletools import TAKEN_FROM_ARGUMENT1
 
 
 class Jugador:
-    def __init__(self, ficha):
-        self.ficha = ficha 
+    def __init__(self, numero):
+        self.numero = numero
+        self.ganador = False
 
+    def añadir_ficha(self,ficha):
+        self.ficha = ficha
 
+    def ficha(self):
+        return self.ficha
 
 class Tablero:
     def __init__(self, tamaño):
-        self.matriz = ['-'] * tamaño
-        for i in range(len(self.matriz)):
-            self.matriz[i] = ['-'] * tamaño
-            print
-
-    def imprimir_tablero(self): 
+        self.matriz = []
+        for i in range(tamaño):
+            self.matriz.append(['-'] * tamaño)
+        print(self.matriz)
+    
+    def imprimir(self): 
+        print('---+' * len(self.matriz))
         for i in range(len(self.matriz)):
             for j in range(len(self.matriz[0])):
-                print(self.matriz[i][j], end=" | ")
+                print(f' {self.matriz[i][j]} ', end="|")
             print()
-            
+            print('---+' * len(self.matriz))
+        print()
+    
+    def consultar_matriz(self):
+        return self.matriz
 
+    def movimiento_correcto(self, x, y):
+        if x >= 0 and x < len(self.matriz) and y >= 0 and y < len(self.matriz[0]):
+            if self.matriz[x][y] == 'X' or self.matriz[x][y] == 'O':
+                return False
+            return True
+        return False
+
+    def movimiento(self, x, y, jugador):
+        self.matriz[x][y] = jugador.ficha
+
+    def comprobar_diagonal_principal(self, jugador):
+        pass
+    def comprobar_diagonal_secundaria(self, jugador):
+        pass
+    def comprobar_filas(self, jugador):
+        pass
+    def comprobar_columnas(self,jugador)
         pass
--- main.py
+++ main.py
@@ -1,45 +1,103 @@
+import random
 from clases import Jugador, Tablero
 
-def movimiento_aleatorio():
-    pass
 
-def movimiento_ficha(x,y,jugador):
-    pass
+def movimiento_jugador(x,y,jugador, tablero):
+    # Si no es la maquina el numero es 1, la maquina siempre tiene el número 2.
+    if jugador.numero == 1:  
+        if tablero.movimiento_correcto(x,y) == False:
+            x = int(input(f'Introduce un número de la fila correcto, del 0 al {len(tablero.matriz) - 1}: '))
+            y = int(input(f'Introduce un número de la columna correcto, del 0 al {len(tablero.matriz) - 1}:'))
+            while tablero.movimiento_correcto(x,y) != True:
+                x = int(input(f'Introduce un número de la fila correcto, del 0 al {len(tablero.matriz) - 1}: '))
+                y = int(input(f'Introduce un número de la columna correcto, del 0 al {len(tablero.matriz) - 1}:'))
+            print(tablero.matriz)
+            tablero.movimiento(x,y,jugador)
+        else:
+            tablero.movimiento(x,y,jugador)
+    else:
+        if tablero.movimiento_correcto(x,y) == False:
+            x = random.randint(0,len(tablero.matriz) - 1)
+            y = random.randint(0,len(tablero.matriz) - 1)
+            while tablero.movimiento_correcto(x,y) != True:
+                x = random.randint(0,len(tablero.matriz) - 1)
+                y = random.randint(0,len(tablero.matriz) - 1)
+            tablero.movimiento(x,y,jugador)
+        else:
+            tablero.movimiento(x,y,jugador)
+        
 
-def comprobación_final_juego():
-    pass
 
+def comprobación_ganador(tablero, jugador):
+    if tablero.comprobar_diagonal_principal(jugador) or tablero.comprobar_diagonal_secundaria(jugador) or tablero.comprobar_filas(jugador) or tablero.comprobar_columnas(jugador):
+        return True
+    
 def error(mensaje):
     print(mensaje)
     exit()
 
 def main():
-    print('Bienevenido al tres en ralla juego, donde puedes elegir el tamaño del tablero. ')
+    print('Bienvenido al tres en ralla, donde puedes elegir el tamaño del tablero.\n ')
+
     try: 
         tamaño_tablero = int(input('Introduce el tamaño del tablero: '))
     except:
-        error('No ha introducido un número entero, porfavor reinicie el juevo para volver a intentarlo.')
+        error('No ha introducido un número entero, porfavor reinicie el juego para volver a intentarlo.')
 
     ficha_jugador_1 = input('El jugador 1 quiere jugar con ficha "X" o "O" ?: ')
 
     if ficha_jugador_1 == "X":
-        ficha_jugador_2 = "O"
-        print('Por lo tanto el jugador 2 será la ficha "O"')
-    elif ficha_jugador_1 == "O":
-        ficha_jugador_2 = "X"
-        print('Por lo tanto el jugador 2 será la ficha "X"')
-    else: 
-        error('No ha introducido una ficha valida, porfavor reinicie el juevo para volver a intentarlo. ')
-        
-    jugador1 = Jugador(ficha_jugador_1) 
-    jugador2 = Jugador(ficha_jugador_2) 
+        fichas = ['X','O']
+        print('Por lo tanto el jugador 2, la maquina será la ficha "O"')
 
+    elif ficha_jugador_1 == "O":
+        fichas = ['O','X']
+        print('Por lo tanto el jugador 2, la maquina será la ficha "X"')
+
+    else: 
+        error('No ha introducido una ficha valida, porfavor reinicie el juego para volver a intentarlo. ')
+    
+    # Se crean los jugadores con sus respectivas fichas. 
+
+    jugadores = []
+    for i in range(1, 3): jugadores.append(Jugador(i))
+    for i in range(len(jugadores)): jugadores[i].añadir_ficha(fichas[i])
+
+    # Se crea el tablero. 
+    
     tablero = Tablero(tamaño_tablero)
 
-    print("El tablero creado ha sido el siguiente: ")
-    tablero.imprimir_tablero()
+    print("El tablero creado ha sido el siguiente: \n")
+    tablero.imprimir()
 
+    hay_ganador = False
+    # Empieza el juego. 
 
+    while hay_ganador == False:
+        for i in range(len(jugadores)):
+            print(f'Turno del jugador {jugadores[i].numero}: ')
+            if i == 0: 
+                try:
+                    fila = int(input(f'Introduce el número de la fila, del 0 al {tamaño_tablero - 1}: '))
+                    columna = int(input(f'Introduce el número de la columna, del 0 al {tamaño_tablero - 1}: '))
+                    print()    
+                except:
+                    error('No ha introducido un número entero, porfavor reinicie el juego para volver a intentarlo.')
+                movimiento_jugador(fila,columna,jugadores[i],tablero)
+                
+                if comprobación_ganador(tablero, jugadores[i]):
+                    hay_ganador = True
+            else:
+                movimiento_jugador(fila,columna,jugadores[i],tablero)
+                if comprobación_ganador(tablero, jugadores[i]):
+                    hay_ganador = True
+            tablero.imprimir()
 
-
+    else:
+        for jugador in jugadores:
+            if jugador.ganador == True:
+                print(f'El jugador {jugador.numero} ha ganado, felicidades. ')
+                print(f'El tablero, ha quedado finalmente de la siguiente manera: ')
+                tablero.imprimir()
+                
 main()
